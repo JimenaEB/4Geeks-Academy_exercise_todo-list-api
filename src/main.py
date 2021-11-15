23be0633eb6ef07430235fbaf02a8f0230cc6f17
Jimena Escobar Bermúdez <jime.ebermudez@gmail.com>
2021-11-15 14:55:59 +0000

    Update tasks

--- src/main.py
+++ src/main.py
@@ -2,19 +2,22 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from sqlalchemy import exc
+
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Task
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,14 +33,113 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    users = User.get_all()
+    all_users = [user.to_dict() for user in users]
+    return jsonify(all_users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user_by_id(id):
+    user = User.get_by_id(id)
+
+    if user:
+        return jsonify(user.to_dict()), 200
+    
+        return jsonify({'error': "User not found"}), 404
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    new_nick = request.json.get('nick', None)
+
+    if not new_nick:
+        return jsonify({'error': 'Missing parameters'}), 400
+
+    user = User(nick=new_nick, _is_active=True)
+    try:
+        user.create()
+        return jsonify(user_created.to_dict()), 201
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in data'}), 400
+
+
+@app.route('/user/<int:id>', methods=['PUT', 'PATCH'])
+def update_user(id):
+    new_nick = request.json.get('nick', None)
+
+    if not new_nick:
+        return jsonify({'error': 'Missing parameters'}), 400
+
+    user = User.get_by_id(id)
+    if user:
+        user = user.update(new_nick)
+        return jsonify(user.to_dict()), 200
+
+    return jsonify({'error': 'User not found'}), 404
+
+
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.get_by_id(id)
+    if user:
+        user.soft_delete()
+        return jsonify(user.to_dict()), 200
+
+    return jsonify({'error': 'User not found'}), 404
+
+
+@app.route('/user/<int:id>/task', methods=['GET'])
+def get_user_tasks(id):
+    user = User.get_by_id(id)
+
+    if user:
+        tasks = Task.get_by_user(id)
+        tasks_dict = [task.to_dict() for task in tasks]
+        
+        return jsonify(tasks_dict), 200
+
+    return jsonify({'error': 'User not found'}), 404
+
+
+@app.route('/user/<int:id>/task', methods=['POST'])
+def create_user_task(id):
+    user = User.get_by_id(id)
+    label = request.json.get('label', None)
+
+    if user and label:
+        task = Task(label=label, done=False, user_id=id)
+        try:
+            task.create()
+            return jsonify(task.to_dict()), 201
+        except exc.IntegrityError:
+            return jsonify({'error': 'Fail in data'}), 400
+
+    return jsonify({'error': 'User not found or label is missing'}), 404
+
+
+@app.route('/user/<int:id_user>/task/<int:id_task>', methods=['DELETE'])
+def delete_user_task(id_user, id_task):
+    user = User.get_by_id(id_user)
+
+    if user:
+        task = Task.get_by_id(id_task)
+
+        if task:
+            try:
+                task.delete()
+                return jsonify(task.to_dict()), 200
+            except exc.IntegrityError:
+                return jsonify("Fail in operation"), 400
+        
+        return jsonify("Task nor found"), 404
+
+    return jsonify("User not found"), 404
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,99 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__: "user"
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    nick = db.Column(db.String(250), unique=True, nullable=False)
+    _is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User is {self.nick}, id: {self.id}'
 
-    def serialize(self):
+
+    def to_dict(self):
+        tasks = Task.get_by_user(self.id)
+
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "nick": self.nick,
+            "tasks": [task.to_dict() for task in tasks]
         }
+
+
+    @classmethod
+    def get_all(cls):
+        users = cls.query.all()
+        return users
+
+
+    @classmethod
+    def get_by_id(cls,id_user):
+        user = cls.query.filter_by(id=id_user).one_or_none()
+        return user
+
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    
+    def update(self, nick):
+        self.nick = nick
+        db.session.commit()
+        return self
+
+    
+    def soft_delete(self):
+        self._is_active = False
+        db.session.commit()
+        return self
+
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Task(db.Model):
+    __tablename__: "task"
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(250), unique=False, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+    def __repr__(self):
+        return f'Task is {self.label}, {self.done}, from user {self.user_id}, id: {self.id}'    
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'label': self.label,
+            'done': self.done,
+            'user_id': self.user_id
+        }
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        task = cls.query.get(id)
+        return task 
+
+    @classmethod
+    def get_by_user(cls, user_id):
+        tasks = cls.query.filter_by(user_id=user_id)
+        return tasks
+
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
